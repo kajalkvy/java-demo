b23717ffa8338a26844a77211ac908ebe6439359
Kajal <kajal.kvy@gmail.com>
2021-12-04 12:06:19 +0530

    program to get next greater element and updated gitignore file

--- src/com/dataStructure/NextGreaterElement.java
+++ src/com/dataStructure/NextGreaterElement.java
@@ -1,13 +1,26 @@
 package com.dataStructure;
 
+import java.util.Arrays;
+
 public class NextGreaterElement {
     public static void main(String[] args) {
-        int arr[]={4,7,3,4,8,1};
-        int newArray[]=new int[arr.length];
-        for(int i=0;i< arr.length;i++){
-            for(int j=i+1;j< arr.length;i++){
-                if()
+        int arr[] = {4, 7, 3, 4, 8, 1};
+        boolean isGreaterFound = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            isGreaterFound = false;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    arr[i] = arr[j];
+                    isGreaterFound = true;
+                    break;
+                }
+            }
+            if (!isGreaterFound) {
+                arr[i] = -1;
             }
         }
+        arr[arr.length - 1] = -1;
+
+        System.out.println(Arrays.toString(arr));
     }
 }
